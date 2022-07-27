e4ce1b4b9d05e8c6ebf63ab357875396e0c1c9f6
plusforum <103184903+plusforum@users.noreply.github.com>
2022-07-27 15:45:10 +0530

    Update kube-cluster-install.sh

--- kube-cluster-install.sh
+++ kube-cluster-install.sh
@@ -69,16 +69,16 @@ systemctl restart kubelet && systemctl enable kubelet
 kubeadm init --ignore-preflight-errors=all
 
 # configure user environment
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-export kubever=$(kubectl version | base64 | tr -d '\n')
+#mkdir -p $HOME/.kube
+#cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#chown $(id -u):$(id -g) $HOME/.kube/config
+#export kubever=$(kubectl version | base64 | tr -d '\n')
 
 # deploy networking service in Cluster
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 # validate kube cluster components
-kubectl get nodes
+#kubectl get nodes
 
 # Taint Master node 
-kubectl taint nodes --all node-role.kubernetes.io/master-  
+#kubectl taint nodes --all node-role.kubernetes.io/master-  
